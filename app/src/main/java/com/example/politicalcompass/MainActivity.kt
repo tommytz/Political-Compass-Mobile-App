b72283db347e7eb5add2e44008d73cdf7b03c0a3
Damon <murphydamo@myvuw.ac.nz>
2021-10-19 13:33:10 +1300

    initial commit

---
+++ app/src/main/java/com/example/politicalcompass/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.politicalcompass
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.cardview.widget.CardView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    val play : CardView = findViewById(R.id.startQuiz)
+    val button : Button = findViewById(R.id.quizButton)
+    button.setOnClickListener{
+    }
+
+    fun startQuiz(view : View){
+        val quizActivityIntent: Intent = Intent(this@MainActivity, QuizActivity::class.java)
+        startActivity(quizActivityIntent)
+    }
+}
---
+++ app/src/main/java/com/example/politicalcompass/quizActivity.kt
@@ -0,0 +1,14 @@
+package com.example.politicalcompass
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class QuizActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quiz_activity)
+
+        }
+
+}
