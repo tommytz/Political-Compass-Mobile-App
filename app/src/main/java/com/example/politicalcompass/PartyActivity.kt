81397d59edbf518afb6171b76c1466631af44ad3
tardithom <tardithom@myvuw.ac.nz>
2021-10-21 10:48:05 +1300

    Very janky fragment changer don't copy

--- app/src/main/java/com/example/politicalcompass/PartyActivity.kt
+++ app/src/main/java/com/example/politicalcompass/PartyActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
 
@@ -12,6 +13,13 @@ class PartyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.party_activity)
+
+        fun replaceFragment(fragment : Fragment){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+            fragmentTransaction.commit()
+        }
 
         val partyList = listOf<String>("Labour", "National", "Green", "ACT", "Maori")
         val output : TextView = findViewById(R.id.textView)
@@ -25,7 +33,10 @@ class PartyActivity : AppCompatActivity() {
                 adapterView: AdapterView<*>?,
                 view: View?,
                 position: Int,
-                l: Long -> output.text = partyList[position] + " is selected!"
+                l: Long -> if (position == 0){
+                    replaceFragment(LabourPartyInfo())
+            } else if (position == 1){ replaceFragment(NationalPartyInfo())
+                    }
         }
     }
 }
