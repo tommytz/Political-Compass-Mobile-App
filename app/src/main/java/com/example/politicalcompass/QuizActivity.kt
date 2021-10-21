daaf50537b9e94910bb2633044698105c878299a
tardithom <T7q22962459>
2021-10-21 19:47:29 +1300

    Cleaned up quiz xml and added logic for radio button selected

--- app/src/main/java/com/example/politicalcompass/QuizActivity.kt
+++ app/src/main/java/com/example/politicalcompass/QuizActivity.kt
@@ -1,29 +1,26 @@
 package com.example.politicalcompass
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class QuizActivity : AppCompatActivity(){
-    val nextButton : Button = findViewById<Button>(R.id.next_btn)
-
-    val radioGroup : RadioGroup = findViewById(R.id.radio_group)
-
-    private val strongAgree : RadioButton = findViewById(R.id.strong_agree)
-    private val agree : RadioButton = findViewById(R.id.agree)
-    private val neutral : RadioButton = findViewById(R.id.neutral)
-    private val disagree : RadioButton = findViewById(R.id.disagree)
-    private val strongDisagree : RadioButton = findViewById(R.id.strong_disagree)
-
-    val answers = listOf<RadioButton>(strongAgree, agree, neutral, disagree, strongDisagree)
+class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quiz_activity)
+    }
 
-        }
+    /* This function is called by the next button in the quiz xml. It takes the input from the radio group and then unchecks the buttons for the next question.*/
+    fun nextQuestion(view: View) {
+        val radioGroup: RadioGroup = findViewById(R.id.radio_group)
 
+        val selectedRadioButtonId: Int = radioGroup.checkedRadioButtonId
+        val checkedRadioButton: RadioButton = findViewById(selectedRadioButtonId)
+        Toast.makeText(this, checkedRadioButton.text, Toast.LENGTH_SHORT).show()
+        radioGroup.clearCheck()
+    }
 }
