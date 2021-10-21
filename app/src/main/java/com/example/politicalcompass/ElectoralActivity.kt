89433137f0fd297fe046bceb87690c9f8cdf7f9e
damon <murphydamo@myvuw.ac.nz>
2021-10-21 18:29:19 +1300

    feat: add ability to selected island
    added the ability to select between the islands with the code now saving the currently selected path. Will attach this to newly made button action.

--- app/src/main/java/com/example/politicalcompass/ElectoralActivity.kt
+++ app/src/main/java/com/example/politicalcompass/ElectoralActivity.kt
@@ -1,5 +1,6 @@
 package com.example.politicalcompass
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,21 +12,29 @@ import com.richpath.RichPathView
 
 class ElectoralActivity : AppCompatActivity() {
 
+    val pathArray  = mutableListOf<RichPath>()
+    var currentPath : RichPath? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.electoral_map_one)
 
 
         val richPathView: RichPathView = findViewById<RichPathView>(R.id.nz_map)
-
         richPathView.setOnPathClickListener { richPath ->
-            if (richPath.name == "wellington_region") {
-                if (richPath.strokeColor == 2){
-                    richPath.strokeColor = 3
+            pathArray.add(richPath)
+            currentPath = richPath
+            if (richPath.name == "north_island") {
+                richPath.strokeColor = Color.RED
+            } else if (richPath.name == "south_island") {
+                richPath.strokeColor = Color.RED
+            }
+            for (path in pathArray) {
+                if (path == richPath){
+                    continue
                 } else {
-                    richPath.strokeColor = 2
+                    path.strokeColor = Color.WHITE
                 }
-
             }
         }
 
