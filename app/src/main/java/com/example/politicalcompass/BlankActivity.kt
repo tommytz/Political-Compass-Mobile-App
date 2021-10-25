51ff5c868946147a19c13a27c8c43b80b024f32e
damon <murphydamo@myvuw.ac.nz>
2021-10-25 16:03:54 +1300

    bug: trying to get language change to work
    Signed-off-by: damon <murphydamo@myvuw.ac.nz>

--- app/src/main/java/com/example/politicalcompass/BlankActivity.kt
+++ app/src/main/java/com/example/politicalcompass/BlankActivity.kt
@@ -1,5 +1,7 @@
 package com.example.politicalcompass
 
+import android.app.Activity
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.ContextThemeWrapper
@@ -11,9 +13,11 @@ class BlankActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadLocale()
         setContentView(R.layout.blank_layout)
         supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment())
             .commit()
+
 
 //        val themeArray = resources.getStringArray(R.array.theme_values)
 //        val languageArray = resources.getStringArray(R.array.language_values)
@@ -21,6 +25,26 @@ class BlankActivity : AppCompatActivity() {
 //        val themePreference = getPreference("theme_radio")
 
 
+    }
+
+    fun setLocale(language: String) {
+        val locale: Locale = Locale(language)
+        Locale.setDefault(locale)
+        val config: Configuration = Configuration()
+        val resources = baseContext.resources
+        config.locale = locale
+        resources.updateConfiguration(config, resources.displayMetrics)
+        val sharedPrefEdit: SharedPreferences.Editor =
+            getSharedPreferences("settings", MODE_PRIVATE).edit()
+        sharedPrefEdit.putString("language_select", language)
+        sharedPrefEdit.apply()
+
+    }
+
+    fun loadLocale() {
+        val prefs: SharedPreferences = getSharedPreferences("settings", Activity.MODE_PRIVATE)
+        val language: String = prefs.getString("language_select", "").toString()
+        setLocale(language)
     }
 
 //    companion object {
--- app/src/main/java/com/example/politicalcompass/SettingsFragment.kt
+++ app/src/main/java/com/example/politicalcompass/SettingsFragment.kt
@@ -1,6 +1,12 @@
 package com.example.politicalcompass
 
+import android.app.Activity
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityCompat.recreate
 import androidx.preference.DropDownPreference
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
@@ -13,23 +19,43 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        val themeArray = resources.getStringArray(R.array.theme_values)
-        val languageArray = resources.getStringArray(R.array.language_values)
-        val preferences = preferenceManager
 
-        val themePreference = preferences.findPreference<ListPreference>("theme_radio")
-        val languagePreference = preferences.findPreference<DropDownPreference>("language_dropdown")
+    }
 
-        var change = ""
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
-        val language = sharedPreferences.getString("language", "bak")
-        if (language == "English") {
-            change = "english"
-        } else if (language == "Te Reo Maori") {
-            change = "te_reo_maori"
-        } else {
-            change = ""
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val preferenceManager = preferenceManager
+        var themePreference = preferenceManager.findPreference<ListPreference>("theme_radio")
+        var languagePreference =
+            preferenceManager.findPreference<DropDownPreference>("language_dropdown")
+//        loadLocale()
+        languagePreference!!.setOnPreferenceChangeListener { preference, newValue ->
+            BlankActivity().setLocale(newValue as String) //apparently this isn't the right way to do it
+            recreate(BlankActivity())
+            //use the no sound video to add methods here to change the language
+            return@setOnPreferenceChangeListener true
         }
+
+
+//        val themeArray = resources.getStringArray(R.array.theme_values)
+//        val languageArray = resources.getStringArray(R.array.language_values)
+//        val preferences = preferenceManager
+//
+//        val themePreference = preferences.findPreference<ListPreference>("theme_radio")
+//        val languagePreference = preferences.findPreference<DropDownPreference>("language_dropdown")
+//
+//        var change = ""
+//        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
+//        val language = sharedPreferences.getString("language", "bak")
+//        if (language == "English") {
+//            change = "english"
+//        } else if (language == "Te Reo Maori") {
+//            change = "te_reo_maori"
+//        } else {
+//            change = ""
+//        }
 
 
 //        BlankActivity.dLocale = Locale(change)
@@ -50,4 +76,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            }
 //        })
     }
+
+//    fun setLocale(language : String) {
+//        val locale : Locale = Locale(language)
+//        Locale.setDefault(locale)
+//        val config : Configuration = Configuration()
+//        config.locale = locale
+//        BlankActivity().baseContext.resources.updateConfiguration(config,  BlankActivity().baseContext.resources.displayMetrics)
+//        val sharedPrefEdit : SharedPreferences.Editor = BlankActivity().getSharedPreferences("settings", MODE_PRIVATE).edit()
+//        sharedPrefEdit.putString("language_select", language)
+//        sharedPrefEdit.apply()
+//
+//    }
+//
+//    fun loadLocale() {
+//        val prefs : SharedPreferences = BlankActivity().getSharedPreferences("settings", Activity.MODE_PRIVATE)
+//        val language : String = prefs.getString("language_select", "").toString()
+//        setLocale(language)
+//    }
 }
